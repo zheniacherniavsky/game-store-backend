e8ea8813c9980f2a5cb24f7d8b0a3b410953aa83
Yauheni Charniauski <zheniacherniavskyplay@gmail.com>
2021-08-10 16:34:35 +0300

    created simple server with get method handler

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const port = 3000;
+
+app.get('/products', (req, res) => {
+  fs.readFile("./data/games.json", (err, data) => {
+    if(err) return res.status(500).send("Server error")
+    
+    try
+    {
+    data = JSON.parse(data);
+    } catch(objError) {
+      console.error(objError.message);
+      return res.status(500).send('Server error');
+    }
+
+    res.status(200).json(data);
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
