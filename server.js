8051fcfd8cfc34af677680d93c94c26b31e1e358
Yauheni <zheniacherniavskyplay@gmail.com>
2021-08-11 00:47:47 +0300

    removed file reader

--- server.js
+++ server.js
@@ -5,16 +5,12 @@ const app = express();
 const port = 3000;
 
 app.get('/products', (req, res) => {
-  fs.readFile("./data/games.json", (err, data) => {
-    if(err) return res.status(500).send("Server error")
-    
-    database.getAllProducts().then(products => {
-      res.status(200).json(products);
-    }, err => {
-      console.log(err);
-      res.status(500).send("Server error");
-    });
-  })
+  database.getAllProducts().then(products => {
+    res.status(200).json(products);
+  }, err => {
+    console.log(err);
+    res.status(500).send("Server error");
+  });
 })
 
 app.listen(port, () => {
