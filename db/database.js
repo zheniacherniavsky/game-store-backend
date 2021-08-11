b9fb507e8ee05e30d0b4427e043e666daa20dfa8
Yauheni <zheniacherniavskyplay@gmail.com>
2021-08-11 22:49:47 +0300

    created init and dbLog functions

--- db/database.js
+++ db/database.js
@@ -1,26 +1,15 @@
 const mongoose = require('mongoose');
-const { Product } = require('./models/product');
+const { dbLog, init } = require('./common');
 
-const connect = () => {
+const connectMongoDb = () => {
   mongoose.connect('mongodb://localhost:27017/itechart');
   const db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error:'));
   db.once('open', () => {
-    console.log('connected to mongodb');
+    dbLog('Connection', 'Connected to MongoDB');
   });
 }
 
-const getAllProducts = () => {
-  return new Promise((resolve, reject) => {
-    Product.find({}, (err, products) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(products);
-      }
-    });
-  });
-}
-
-module.exports.connect = connect;
-module.exports.getAllProducts = getAllProducts;
+module.exports.connect = connectMongoDb;
+module.exports.init = init;
+module.exports.products = require('./operations/products');
