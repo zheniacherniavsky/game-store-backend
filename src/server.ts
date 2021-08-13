d7ac6b951be5d6a33152aa4bd04ef5167898e38c
Yauheni Charniauski <zheniacherniavskyplay@gmail.com>
2021-08-13 14:56:41 +0300

    added product router

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
-import express from "express"
+import express from 'express';
 import database from "./db";
+import { ProductDA } from "./DA";
+import { ProductRouter } from "./routes/product.routes";
+import { ProductService } from "./service";
 const app = express();
 const port = 3000;
 
 database.connect();
 
-app.get('/products', async (req, res) => {
-  
-})
+const router = express.Router();
+app.use('/', router)
+
+ProductRouter(router, new ProductService(new ProductDA));
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
