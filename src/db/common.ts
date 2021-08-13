fcd105de01ed6bd0b6c16b17ca7c48bcf13f54bd
Yauheni Charniauski <zheniacherniavskyplay@gmail.com>
2021-08-13 12:25:23 +0300

    installed ts-node-dev package, updated run script and common.ts imports

--- src/db/common.ts
+++ src/db/common.ts
@@ -1,42 +1,40 @@
-import Category from "./models/category"
-import Product from "./models/product"
+import { CategoryModel } from "./models/category"
+import { ProductModel } from "./models/product"
 
 export const dbLog = (operation: string, msg: string) => console.log(`MongoDB [${operation}]: ${msg}`);
 
 export const init = () => {
-  Category.find({}).then(categories => {
+  CategoryModel.find().then((categories) => {
     if (categories.length === 0) {
-      dbLog("Initialization", "No categories found, creating default categories with products...");
+      dbLog(
+        'Initialization',
+        'No categories found, creating default categories with products...',
+      );
 
-      const shooter = new Category({ displayName: "Shooter" })
-      const arcade = new Category({ displayName: 'Arcade' })
-      const board = new Category({ displayName: 'Board'})
+      const shooter = new CategoryModel({ displayName: 'Shooter' });
+      const arcade = new CategoryModel({ displayName: 'Arcade' });
+      const board = new CategoryModel({ displayName: 'Board' });
       shooter.save();
       arcade.save();
       board.save();
-      
-      new Product({
+
+      new ProductModel({
         displayName: 'Battlefield 4',
-        categoryIds: [
-          shooter._id,
-          arcade._id,
-        ],
+        categoryIds: [shooter._id, arcade._id],
         createdAt: new Date().toString(),
         totalRating: 8,
         price: 29,
       }).save();
 
-      new Product({
-        displayName: "Chess",
-        categoryIds: [
-          board._id,
-        ],
+      new ProductModel({
+        displayName: 'Chess',
+        categoryIds: [board._id],
         createdAt: new Date(),
         totalRating: 8,
-        price: 0
+        price: 0,
       }).save();
 
       dbLog('Initialization', 'Data initialized!');
     }
-  })
+  });
 }
