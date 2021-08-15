b4739565b1cf06cdcb02dde22b6c7d19922d0fe9
Yauheni <zheniacherniavskyplay@gmail.com>
2021-08-15 20:27:21 +0300

    added repository interfaces

--- src/types/types.ts
+++ src/types/types.ts
@@ -15,3 +15,26 @@ export interface ICategory {
 export interface IDatabaseModelOperations<T> {
   getAll: () => Promise<T[]>;
 }
+
+export interface IProductRepository<T, K> {
+  getAll: () => Promise<T[]>;
+  getById: (id: K) => Promise<T>;
+}
+
+export interface ICategoryRepository<T, K> {
+  getAll: () => Promise<T[]>;
+  getById: (id: K) => Promise<T>;
+}
+
+/* eslint-disable @typescript-eslint/no-empty-interface */
+export interface IProductTypeOrmRepository
+  extends IProductRepository<IProduct, number> {}
+
+export interface IProductTypegooseRepository
+  extends IProductRepository<IProduct, ObjectId> {}
+
+export interface ICategoryTypeOrmRepository
+  extends ICategoryRepository<ICategory, number> {}
+
+export interface ICategoryTypegooseRepository
+  extends ICategoryRepository<ICategory, ObjectId> {}
