240d81b077cc57afbea90cc753d73f6adb0cd054
Yauheni <zheniacherniavskyplay@gmail.com>
2021-08-15 10:36:31 +0300

    deleted dist folder and added to gitignore

--- dist/DA/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ProductDA = void 0;
-const ProductDA_1 = require("./ProductDA");
-Object.defineProperty(exports, "ProductDA", { enumerable: true, get: function () { return ProductDA_1.ProductDA; } });
-//# sourceMappingURL=index.js.map
--- dist/db/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const common_1 = require("./common");
-const database = {
-    connect: common_1.connectMongoDb,
-    init: common_1.init
-};
-exports.default = database;
-//# sourceMappingURL=index.js.map
