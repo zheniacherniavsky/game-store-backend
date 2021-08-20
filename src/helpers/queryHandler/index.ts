25b93b2f304ed4e90bacc24f41dd01ce85a57508
Yauheni Charniauski <zheniacherniavskyplay@gmail.com>
2021-08-20 17:54:48 +0300

    added product price search option

--- src/helpers/queryHandler/index.ts
+++ src/helpers/queryHandler/index.ts
@@ -1,16 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { productSearchQueryHandler } from "./product";
+import { productSearchQueryHandler } from './product';
 
 export interface Result {
   typegooseOptions: {
-    find: any
-  },
-  typeOrmOptions: any
+    find: any;
+  };
+  typeOrmOptions: any;
 }
 
 export interface QueryObject {
-  displayName?: string
-  minRating?: number
+  displayName?: string;
+  minRating?: number;
+  price?: string;
 }
 
-export {productSearchQueryHandler};
+export { productSearchQueryHandler };
--- src/helpers/queryHandler/product.ts
+++ src/helpers/queryHandler/product.ts
@@ -1,31 +1,43 @@
-import { QueryObject, Result } from ".";
-import { ILike, MoreThanOrEqual } from "typeorm";
+import { QueryObject, Result } from '.';
+import { Between, ILike, MoreThanOrEqual } from 'typeorm';
 
 export const productSearchQueryHandler = (query?: QueryObject): Result => {
   const res: Result = {
     typegooseOptions: {
-      find: {}
+      find: {},
     },
-    typeOrmOptions: {}
+    typeOrmOptions: {},
   };
 
-  if(query === undefined) return res;
+  if (query === undefined) return res;
 
-  if(query.displayName !== undefined)
-  {
+  if (query.displayName !== undefined) {
     res.typegooseOptions.find.displayName = {
       $regex: '.*' + query.displayName + '.*',
-      $options: 'i'
+      $options: 'i',
     };
     res.typeOrmOptions.displayName = ILike(`%${query.displayName}%`);
   }
 
-  if(query.minRating !== undefined)
-  {
+  if (query.minRating !== undefined) {
     res.typegooseOptions.find.totalRating = {
-      $gte: query.minRating
+      $gte: query.minRating,
     };
     res.typeOrmOptions.totalRating = MoreThanOrEqual(query.minRating);
+  }
+
+  if (query.price !== undefined) {
+    const regex = new RegExp(/[0-9]*:[0-9]+/gm);
+    const price = query.price.match(regex);
+    if (price !== null) {
+      const [min, max] = price[0].split(':').map((value) => parseInt(value));
+
+      res.typegooseOptions.find.price = {
+        $gte: isNaN(min) ? 0 : min,
+        $lte: max,
+      };
+      res.typeOrmOptions.price = Between(isNaN(min) ? 0 : min, max);
+    }
   }
 
   return res;
