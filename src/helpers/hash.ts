ba5c9e9e4b1c619ab603c761186e505bb7dc51af
Yauheni Charniauski <zheniacherniavskyplay@gmail.com>
2021-08-27 13:23:05 +0300

    installed bcrypt, created hash helper

---
+++ src/helpers/hash.ts
@@ -0,0 +1,15 @@
+import bcrypt from 'bcrypt';
+
+const saltRounds = 12;
+
+export const hashData = async (data: string): Promise<string> => {
+  const hashedData = await bcrypt.hash(data, saltRounds);
+  return hashedData;
+};
+
+export const compareHashedData = async (
+  data: string,
+  hashedData: string,
+): Promise<boolean> => {
+  return await bcrypt.compare(data, hashedData);
+};
