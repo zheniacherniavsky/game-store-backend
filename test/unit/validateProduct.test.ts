21b541c1debf4eba8dc5b0c01d0a81ffe781bb38
Yauheni Charniauski <zheniacherniavskyplay@gmail.com>
2021-12-29 17:29:52 +0300

    added unit tests for product validation

---
+++ test/unit/validateProduct.test.ts
@@ -0,0 +1,25 @@
+import { validateProduct } from '../../src/helpers/validation';
+
+test('Throws error when displayName is invalid', () => {
+  expect(() => {
+    validateProduct({
+      displayName: '',
+      price: 10,
+      createdAt: new Date(),
+      ratings: [],
+      totalRating: 0,
+    });
+  }).toThrowError();
+});
+
+test('Returns undefined if product is correct', () => {
+  expect(() => {
+    validateProduct({
+      displayName: 'Hello',
+      price: 10,
+      createdAt: new Date(),
+      ratings: [],
+      totalRating: 0,
+    });
+  }).toBeUndefined;
+});
