f04f378f0d2ffa0f4f7ed89edcb9cd25dce4c65c
Yauheni Charniauski <zheniacherniavskyplay@gmail.com>
2021-12-29 22:50:25 +0300

    added get products route test

---
+++ test/integrationTests/products.test.ts
@@ -0,0 +1,46 @@
+import express from 'express';
+import request from 'supertest';
+import { ProductRouter } from '../../src/routes/product.routes';
+
+import { ProductRepository } from '../../src/DA';
+const router = express.Router();
+const app = express();
+ProductRouter(router);
+
+app.use('/', router);
+
+const fakeProducts = [
+  {
+    _id: 2,
+    displayName: 'Game 2',
+    createdAt: '2021-11-28T14:18:35.153Z',
+    totalRating: 2,
+    price: 19,
+  },
+  {
+    _id: 3,
+    displayName: 'Game 3',
+    createdAt: '2021-11-28T14:18:35.153Z',
+    totalRating: 2,
+    price: 19,
+  },
+];
+
+jest.mock('../../src/DA', () => {
+  const originalModule = jest.requireActual('../../src/DA');
+
+  return {
+    ...originalModule,
+    ProductRepository: {
+      getProductsList: jest.fn(() => fakeProducts),
+    },
+  };
+});
+
+describe('Testing product routes', () => {
+  test('GET /products - success', async () => {
+    const { body } = await request(app).get('/products');
+
+    expect(body).toEqual(fakeProducts);
+  });
+});
