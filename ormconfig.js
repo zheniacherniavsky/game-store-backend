b1c07c3ee8f3d8db1c1be4e5e9d3322b70168b0e
Yauheni Charniauski <zheniacherniavskyplay@gmail.com>
2022-01-09 18:38:04 +0300

    updated ormconfig with new postgresql options

--- ormconfig.js
+++ ormconfig.js
@@ -16,28 +16,27 @@ const srcConfig = {
   subscribers: [__dirname + '/src/DA/db/postgresql/subscriber/**/*.ts'],
   cli: {
     entitiesDir: __dirname + '/src/DA/db/postgresql/entity',
-    migrationsDir: __dirname + '/src/DA/db/postgresql/migration',
+    migrationsDir: '/src/DA/db/postgresql/migration',
     subscribersDir: __dirname + '/src/DA/db/postgresql/subscriber',
   },
 };
 
 const distConfig = {
   type: 'postgres',
-  host: process.env.PG_DB_HOST,
-  port: process.env.PG_DB_PORT,
-  username: data.PG_DB_USERNAME || '',
-  password: data.PG_DB_PASSWORD || '',
-  database: process.env.PG_DB_DATABASE,
   synchronize: true,
   logging: false,
+  url: process.env.DATABASE_URL,
+  extra: {
+    ssl: true,
+  },
   entities: [__dirname + '/dist/DA/db/postgresql/entity/**/*.js'],
   migrations: [__dirname + '/dist/DA/db/postgresql/migration/**/*.js'],
   subscribers: [__dirname + '/dist/DA/db/postgresql/subscriber/**/*.js'],
   cli: {
     entitiesDir: __dirname + '/dist/DA/db/postgresql/entity',
-    migrationsDir: __dirname + '/dist/DA/db/postgresql/migration',
+    migrationsDir: '/dist/DA/db/postgresql/migration',
     subscribersDir: __dirname + '/dist/DA/db/postgresql/subscriber',
   },
 };
 
-module.exports = process.env.TS_NODE_DEV ? srcConfig : distConfig;
+module.exports = true ? srcConfig : distConfig;
